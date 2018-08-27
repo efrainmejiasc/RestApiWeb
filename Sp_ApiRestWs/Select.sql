b459d8148dfe66ea6ff82b323670c0c646ee91c5
EfrainMejiasC <efrainmejiasc@hotmail.com>
2018-08-27 12:59:07 -0400

    comite

--- Sp_ApiRestWs/Select.sql
+++ Sp_ApiRestWs/Select.sql
@@ -1,3 +1,5 @@
 select * from Cliente order by NUMERO ASC
 select * from ClienteTrack
 SELECT * FROM   SyncTransaccion order by numero asc 
+
+update SyncTransaccion Set Estado ='TERMINADO' WHERE NUMERO = 12
